d2da8480aa93c2f59cdb8ab978ebba437a12fbd1
Usarneme <jamestcoop@gmail.com>
2021-01-31 18:43:20 -0800

    Wrote a basic todo list using Nodes that contain a value and an array of children nodes. Rendering ok. Does not accept blank/empty input.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Node {
+  constructor(value) {
+    this.childNodes = []
+    this.value = value
+  }
 }
 
-export default App;
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = new Node("root")
+  }
+
+  addNode(event) {
+    event.preventDefault()
+    if (event.target[0] === undefined) {
+      // onBlur event fired
+      if (event.target.value.trim() === "") return event.target.value = ""
+      const newNode = new Node(event.target.value.trim())
+      this.setState(prevState => ({
+        childNodes : [
+          ...prevState.childNodes, 
+          newNode 
+        ]
+      }))
+      event.target.value = ""
+    } else { 
+      // the form was submitted, the 0th element is the nodeValue named input
+      if (event.target[0].value.trim() === "") return event.target[0].value = ""
+      const newNode = new Node(event.target[0].value.trim())
+      this.setState(prevState => ({
+        childNodes : [
+          ...prevState.childNodes, 
+          newNode 
+        ]
+      }))
+      event.target[0].value = ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <section>
+          <div>
+            <ul>
+              {
+                this.state.childNodes.length > 0 && 
+                this.state.childNodes.map(child => <li key={child.value}>{child.value} - {child.childNodes.toString()}</li>)
+              }
+              <li>
+                <form onBlur={e => this.addNode(e)} onSubmit={e => this.addNode(e)} >
+                  <input type="text" name="nodeValue" placeholder="Enter new item..." />
+                </form>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <header className="App-header">
+          <h1>Nodepad</h1>        
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
